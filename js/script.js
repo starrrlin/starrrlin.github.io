9fd07b9fdc7019dea49ecf98d23f1d87b3b4724e
starrrlin <xingchenlin@uchicago.edu>
2024-04-29 16:20:19 -0500

    ok

--- js/script.js
+++ js/script.js
@@ -1,3 +1,42 @@
+// Last item function
+function lastItem() {
+    var arrayName = ['Watermelon', 'Apple', 'Orange', 'Kiwi']
+    var y = arrayName.slice();
+    var selectedElement = document.getElementById("fruit");
+    var x = arrayName.sort();
+    selectedElement.innerText = `The original array is [${y}], and I sorted it alphabetically.
+    The last item of the sorted array is ${x[x.length - 1]}.`;
+  }
+  
+// WHERE DO WE CONSTRUCT THE NEW OBJECT?
+const responses = {};
+
+function sortItemsInput() {
+
+    // ASK HOW MANY ITEMS TO ENTER, CHECK THE NUMBER PROVIDED BY THE PROMPT
+    while(isNaN(numItems) || numItems > 4 || numItems < 2 || !(Number.isInteger(numItems))){
+        var numItems = Number(prompt('How many items would you like to enter? 2-4.'))
+    }
+
+    // COLLECT INPUT FROM USER
+    for (let index = 0; index < numItems; index++) {
+        let catInput = prompt(`Enter category ${index+1} of ${numItems}.`)
+        let catItem = prompt(`Enter your ${catInput}`)
+        responses[catInput] = catItem;
+        // We cannot create the responses object here!
+    };
+
+    // IF YOU WANT TO SORT BY KEY
+    const responseKeys = Object.keys(responses).sort();
+
+    for (const key of responseKeys) {
+        let newDiv = document.createElement("div");
+        newDiv.innerHTML = `Your ${key.toLowerCase()} is ${responses[key]}`
+        document.getElementById('outputArray').appendChild(newDiv)
+    }
+};
+
+// Trivia Function
 function checkTrivia() {
     const answer = document.getElementById("triviaAnswer").value;
     const correctAnswer = "Paris";
